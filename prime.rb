6b9df3eff7d30fdcf0234fb79ff4c4f75a02d9aa
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 00:28:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 # Add  code here!
 def prime?(int)
-i=2
-while i<int
-  if (int % i==0)&& (int % i ==
-    return false 
-  end
-    i+=1
-    # elsif int == -1
-    # return false
-  end
-    true
-  end
+# i=2
+# while i<int
+#   if (int % i==0)&& (int % i ==
+#     return false 
+#   end
+#     i+=1
+#     # elsif int == -1
+#     # return false
+#   end
+#     true
+#   end
   
 
   
